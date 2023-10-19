84f664389a7282e897ff12b6e7e584e7a85c5e10
Santosh-2003-sahoo <imsantoshsahoo1430@gmail.com>
2023-10-19 20:41:20 +0530

    uploading files

---
+++ index.js
@@ -0,0 +1,22 @@
+`use strict`;
+
+let numdrumlength = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numdrumlength; i++) {
+  ["click", "mouseover"].forEach((event) => {
+    document.querySelectorAll(`.drum`)[i].addEventListener(event, function () {
+      let buttonInnerHTML = this.innerHTML;
+      var audio = new Audio(`./sounds/${buttonInnerHTML}.mp3`);
+      audio.play();
+      animation(buttonInnerHTML);
+    });
+  });
+}
+
+function animation(currentKey) {
+  let activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
